089308a84fb769e060b9dc204b190b5a6053e6f0
yoavw <yoav@weitzman.co.il>
2018-06-24 15:53:55 +0300

    initial

---
+++ src/includes/config.php
@@ -0,0 +1,8 @@
+<?
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db']['host']   = 'localhost';
+$config['db']['user']   = 'user';
+$config['db']['pass']   = 'password';
+$config['db']['dbname'] = 'exampleapp';
---
+++ src/public/index.php
@@ -0,0 +1,19 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+require 'includes/config.php';
+
+$app = new \Slim\App(['settings' => $config]);
+
+$container = $app->getContainer();
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+$app->run();
